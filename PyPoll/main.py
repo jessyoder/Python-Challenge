8ffced233f3b77fa736f5cee1fa7863641f32551
Jess Yoder <jessica.m.yoder@gmail.com>
2020-07-02 16:46:08 -0400

    Made a little progress on PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,29 +4,43 @@ import csv
 # Set path for file
 csvPath = os.path.join('Resources', "election_data.csv")
 
-votes_count = 0
-
 # Open and read csv file
 with open(csvPath, newline="") as csvFile:
     csvReader = csv.reader(csvFile, delimiter=',')
 
-    # Skip Header Row
-    # Header = next(csvReader)
-    # print(Header)
+    # Create a path to export results to "Analysis" folder
+    # output_file = open("Analysis/Analysis.txt","w+")
 
-    # Your task is to create a Python script that analyzes the votes and calculates each of the following:
-    print("Election Analysis")
-    print("---------------------------------")
+    header = next(csvReader)
+    # first_row = next(csvReader)
+    total_votes = 0
+    total_candidates = []
+    candidates_list = []
+    candidate_dict = {}
+   
+    # Your task is to create a Python script that analyzes the records to calculate each of the following:
+    # print("Election Results")
+    # print("---------------------------------")
 
     for row in csvReader:
-
+    
         # The total number of votes cast
-        votes_count += 1
+        total_votes += 1
 
         # A complete list of candidates who received votes
+        name = row[2] 
+        if name not in candidates_list:
+            candidates_list.append(name)
+            candidate_dict[name] = 0
+        candidate_dict[name] = candidate_dict[name] + 1
 
         # The percentage of votes each candidate won
 
         # The total number of votes each candidate won
 
         # The winner of the election based on popular vote.
+
+    print(f'Total Votes: {total_votes}')
+    print("---------------------------------")
+    for candidate in candidates_list:
+        print(candidate + " has " + str(candidate_dict[candidate]) + " total votes")
