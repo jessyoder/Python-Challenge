aa6736d8258db4eb0c9d1591d834e8fc018bb753
Jess Yoder <jessica.m.yoder@gmail.com>
2020-07-05 17:02:51 -0400

    Adding output to PyPoll main

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,22 +9,20 @@ with open(csvPath, newline="") as csvFile:
     csvReader = csv.reader(csvFile, delimiter=',')
 
     # Create a path to export results to "Analysis" folder
-    # output_file = open("Analysis/Analysis.txt","w+")
+    output_file = open("Analysis/Analysis.txt","w+")
 
     header = next(csvReader)
-    # first_row = next(csvReader)
     total_votes = 0
     total_candidates = []
     candidates_list = []
-    candidate_dict = {}
-    # candidate_votes
+    candidate_votes = {}
     votes_average = []
     winner = ''
     votes = 0
    
     # Your task is to create a Python script that analyzes the records to calculate each of the following:
-    print("Election Results")
-    print("---------------------------------")
+    output_file.write("Election Results\n")
+    output_file.write("---------------------------------\n")
 
     for row in csvReader:
     
@@ -35,25 +33,24 @@ with open(csvPath, newline="") as csvFile:
         name = row[2] 
         if name not in candidates_list:
             candidates_list.append(name)
-            candidate_dict[name] = 0
-        # candidate_dict[name] = candidate_dict[name] + 1
-        candidate_dict[name] += 1
+            candidate_votes[name] = 0
+        candidate_votes[name] += 1
 
-    print(f'Total Votes: {total_votes}')
-    print("---------------------------------")
+    output_file.write(f'Total Votes: {total_votes}\n')
+    output_file.write("---------------------------------\n")
     
      # The total number of votes each candidate won
-    for candidate in candidate_dict:
+    for candidate in candidate_votes:
 
         # The percentage of votes each candidate won
-        votes_average = ((candidate_dict[candidate]* 100) / total_votes)
-        print(f'{candidate}: {votes_average:,.3f}% ({str(candidate_dict[candidate])})')
+        votes_average = ((candidate_votes[candidate]* 100) / total_votes)
+        output_file.write(f'{candidate}: {votes_average:,.3f}% ({str(candidate_votes[candidate])})\n')
         
         # The winner of the election based on popular vote.
-        if votes < candidate_dict.get(candidate):
+        if votes < candidate_votes.get(candidate):
             winner = candidate
-            votes = candidate_dict.get(candidate)
+            votes = candidate_votes.get(candidate)
         
-    print("---------------------------------")
+    output_file.write("---------------------------------\n")
     
-    print(f'Winner: {winner}')
+    output_file.write(f'Winner: {winner}\n')
